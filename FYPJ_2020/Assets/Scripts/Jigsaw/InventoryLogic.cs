e5f41f38735c5f7e6a7c77b671a7249b4b4c2fdc
Derpiderpi <30992043+Derpiderpi@users.noreply.github.com>
2020-07-13 17:32:10 +0800

    jigsaw

--- FYPJ_2020/Assets/Scripts/Jigsaw/InventoryLogic.cs
+++ FYPJ_2020/Assets/Scripts/Jigsaw/InventoryLogic.cs
@@ -98,3 +98,106 @@ public class InventoryLogic : MonoBehaviour
         transform.position = new Vector2(transform.position.x, newY);
     }
 }
+
+
+
+//using System.Collections.Generic;
+//using UnityEngine;
+//using UnityEngine.Rendering;
+
+//public class InventoryLogic : MonoBehaviour
+//{
+//    public static InventoryLogic instance = null;
+//    [Tooltip("The value is a bool which indicates whether the piece is in the inventory")]
+//    [SerializeField] SerializedDictionary inventory;
+//    [Tooltip("The distance between each piece in the inventory")]
+//    [SerializeField] float inventoryOffset;
+//    [SerializeField] float length;
+//    Vector2 treshold;
+//    float minY;
+//    public float maxY;
+
+//    #region Getters & Setters
+//    public SerializedDictionary Inventory
+//    {
+//        get { return inventory; }
+//        set { inventory = value; }
+//    }
+//    public float InventoryOffset
+//    {
+//        get { return inventoryOffset; }
+//        set { inventoryOffset = value; }
+//    }
+//    public Vector2 Treshold
+//    {
+//        get { return treshold; }
+//        set { treshold = value; }
+//    }
+//    #endregion
+
+//    private void Awake()
+//    {
+//        if (GameObject.Find("Inventory") && GameObject.Find("Inventory") != gameObject)
+//        {
+//            Destroy(gameObject);
+//            return;
+//        }
+//        if (instance == null)
+//            instance = this;
+//        else if (instance != this)
+//        {
+//            Destroy(gameObject);
+//            return;
+//        }
+//    }
+
+//    private void Start()
+//    {
+//        length = transform.parent.GetComponent<SpriteRenderer>().size.x;
+//        minY = maxY = transform.position.y;
+//    }
+
+//    public void SetTreshold()
+//    {
+//        treshold = new Vector2(transform.parent.GetComponent<Renderer>().bounds.min.x, transform.parent.position.y);
+//        treshold = Camera.main.WorldToScreenPoint(treshold);
+//    }
+
+//    public void SortInventory()
+//    {
+//        Vector2 pos = transform.position;
+//        int inventoryPieces = 0;
+//        foreach (KeyValuePair<GameObject, bool> piece in inventory)
+//        {
+//            if (!piece.Value) continue;
+
+//            piece.Key.transform.position = pos;
+//            pos.y -= inventoryOffset;
+//            ++inventoryPieces;
+//        }
+//        maxY = inventoryOffset * (inventoryPieces - 2.5f);
+
+//        UpdateSortingOrder();
+//    }
+
+//    public void UpdateSortingOrder()
+//    {
+//        //transform.parent.GetComponent<SortingGroup>().sortingOrder = MouseLogic.instance.SortingOrder;
+//        ++MouseLogic.instance.SortingOrder;
+
+//        foreach (KeyValuePair<GameObject, bool> piece in inventory)
+//        {
+//            if (!piece.Value) continue;
+
+//            MouseLogic.instance.SetSortingOrder(piece.Key);
+//        }
+//    }
+
+//    public void OnMove(Vector2 prev, Vector2 curr)
+//    {
+//        float newY = transform.position.y + (curr.y - prev.y);
+//        if (newY < minY) newY = minY;
+//        else if (newY > maxY) newY = maxY;
+//        transform.position = new Vector2(transform.position.x, newY);
+//    }
+//}
--- FYPJ_2020/Assets/Scripts/Jigsaw/JigsawPieceLogic.cs
+++ FYPJ_2020/Assets/Scripts/Jigsaw/JigsawPieceLogic.cs
@@ -123,3 +123,157 @@ public class JigsawPieceLogic : MonoBehaviour
         }
     }
 }
+
+
+
+
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using UnityEngine.Rendering;
+//using RotaryHeart.Lib.SerializableDictionary;
+
+//[System.Serializable]
+//public class SerializedDictionary : SerializableDictionaryBase<GameObject, bool> { }
+
+//public class JigsawPieceLogic : MonoBehaviour
+//{
+//    public enum PIECE_STATE
+//    {
+//        STATE_NONE,
+//        STATE_ONINVENTORY,
+//        STATE_PICKEDUP,
+//        STATE_PUTDOWN,
+//        STATE_LOCKED,
+//        STATE_ALL
+//    }
+//    [SerializeField] GameObject slot;
+//    [SerializeField] LayerMask originalLayer;
+//    [SerializeField] LayerMask lockedLayer;
+//    [SerializeField] PIECE_STATE state;
+//    [SerializeField] SerializedDictionary neighbours = new SerializedDictionary();
+//    Vector2 offset;
+
+//    #region Getters & Setters
+//    public PIECE_STATE State
+//    {
+//        get { return state; }
+//        set { state = value; }
+//    }
+//    public GameObject Slot
+//    {
+//        get { return slot; }
+//        set { slot = value; }
+//    }
+//    public Vector2 Offset
+//    {
+//        get { return offset; }
+//        set { offset = value; }
+//    }
+//    #endregion
+
+//    private void Awake()
+//    {
+//        originalLayer = gameObject.layer;
+//        lockedLayer = LayerMask.GetMask("Default");
+//        state = PIECE_STATE.STATE_NONE;
+//        offset = Vector2.zero;
+//    }
+
+//    public void CreatePiece(string name)
+//    {
+//        CreateSlot(name);
+//        gameObject.name = "Piece ID:" + name;
+//        //transform.parent = InventoryLogic.instance.transform;
+//        //InventoryLogic.instance.Inventory.Add(gameObject, true);
+
+//        // get which side of the board the puzzle is on
+//        int rand = Random.Range(0, 4);
+//        Vector4 boardEdges = MouseLogic.instance.pieceGenerator.boardEdges;
+//        switch (rand)
+//        {
+//            case 0:
+//                // up
+//                transform.position = new Vector3(Random.Range(boardEdges.x, boardEdges.z), boardEdges.w, transform.position.z);
+//                break;
+//            case 1:
+//                // down
+//                transform.position = new Vector3(Random.Range(boardEdges.x, boardEdges.z), boardEdges.y, transform.position.z);
+//                break;
+//            case 2:
+//                // left
+//                transform.position = new Vector3(boardEdges.x, Random.Range(boardEdges.y, boardEdges.w), transform.position.z);
+//                break;
+//            case 3:
+//                // right
+//                transform.position = new Vector3(boardEdges.z, Random.Range(boardEdges.y, boardEdges.w), transform.position.z);
+//                break;
+//        }
+//    }
+
+//    void Update()
+//    {
+//        switch (state)
+//        {
+//            case PIECE_STATE.STATE_ONINVENTORY:
+//                if (MouseLogic.instance.OnInventory) break;
+//                SwitchState(PIECE_STATE.STATE_PICKEDUP);
+//                break;
+//            case PIECE_STATE.STATE_PICKEDUP:
+//                transform.position = Vector2.Lerp(MouseLogic.instance.MousePos, MouseLogic.instance.MousePos + offset, Vector2.Distance(MouseLogic.instance.MousePos, MouseLogic.instance.MousePos + offset));
+//                break;
+//        }
+//    }
+
+//    void CreateSlot(string name)
+//    {
+//        Color combinedMask = new Color(2, 2, 2, 2);
+//        slot.GetComponent<MeshFilter>().mesh.SetColors(new List<Color>() { combinedMask, combinedMask, combinedMask, combinedMask });
+//        slot.GetComponent<Renderer>().material.color = new Color(0, 0, 0, 1);
+//        slot.transform.parent = null;
+//        slot.transform.position = transform.position;
+//        //slot.transform.localScale.Set(2, 2, 2);
+//        slot.transform.parent = transform.parent;
+//        transform.parent = slot.transform;
+//        slot = transform.parent.gameObject;
+//    }
+
+//    public void SwitchState(PIECE_STATE newState)
+//    {
+//        state = newState;
+//        switch (state)
+//        {
+//            case PIECE_STATE.STATE_PICKEDUP:
+//                //if (InventoryLogic.instance.Inventory.ContainsKey(gameObject))
+//                //    InventoryLogic.instance.Inventory[gameObject] = false;
+//                transform.parent = slot.transform;
+//                transform.localScale = new Vector3(1, 1, 1) * MouseLogic.instance.pieceGenerator.PieceSize;
+//                InventoryLogic.instance.UpdateSortingOrder();
+//                MouseLogic.instance.SetSortingOrder(gameObject);
+//                break;
+//            case PIECE_STATE.STATE_PUTDOWN:
+//                if (Vector2.Distance(transform.position, slot.transform.position) <= 0.5f)
+//                {
+//                    transform.position = slot.transform.position;
+//                    transform.parent = slot.transform;
+//                    state = PIECE_STATE.STATE_LOCKED;
+//                    transform.tag = "Untagged";
+//                    gameObject.layer = lockedLayer;
+//                    GetComponent<SortingGroup>().sortingOrder = 0;
+//                    break;
+//                }
+//                //else if (Camera.main.WorldToScreenPoint(transform.position).x > InventoryLogic.instance.Treshold.x)
+//                //{
+//                //    if (InventoryLogic.instance.Inventory.ContainsKey(gameObject))
+//                //        InventoryLogic.instance.Inventory[gameObject] = true;
+//                //    transform.parent = InventoryLogic.instance.transform;
+//                //    InventoryLogic.instance.SortInventory();
+//                //}
+//                InventoryLogic.instance.UpdateSortingOrder();
+
+//                if (MouseLogic.instance.SelectedPiece == gameObject) MouseLogic.instance.SelectedPiece = null;
+//                state = PIECE_STATE.STATE_NONE;
+//                break;
+//        }
+//    }
+//}
